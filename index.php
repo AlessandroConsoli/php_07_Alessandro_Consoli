081fa7884f17844a027eae995f5cabf440f78f16
Alessandro Consoli <alessandroconsoli.1982@gmail.com>
2024-11-11 19:45:31 +0100

    esercizio completato

---
+++ index.php
@@ -0,0 +1,102 @@
+<?php
+
+$password = readline('Inserisci una password: ');
+
+echo "password inserita: " . $password . "\n";
+
+
+
+
+
+// Regola 1 - Minimo 8 Caratteri
+
+function checkLenght($pwd){
+    if (strlen($pwd) >= 8) {
+        return true;
+    }else{
+        
+        echo "Errore rilevato! La password deve contenere minimo 8 caratteri \n";
+        return false;
+
+    }
+};
+
+// checkLenght($password);
+
+
+
+
+// Regola 2 - Deve contenere almeno un numero
+
+function checkNumber($pwd){
+    for ($i=0; $i < strlen($pwd); $i++) { 
+        if (is_numeric($pwd[$i])){
+            return true;
+        }
+    }
+    echo "Errore rilevato! La password deve contenere almeno 1 numero \n";
+    return false;
+};
+
+// checkNumber($password);
+
+
+
+// Regola 3 - Deve contenere almeno un carattere maiuscolo (ctype_upper)
+
+function checkChars($pwd){
+
+    for ($i=0; $i < strlen($pwd); $i++) { 
+        if (ctype_upper($pwd[$i])) {
+            return true;
+        }
+    }
+    echo "Errore rilevato! La password deve contenere almeno 1 carattere maiuscolo \n";
+    return false;
+}
+
+// checkChars($password);
+
+
+
+// Regola 4 - Deve contenere almeno un carattere speciale 
+
+function checkSpecialChars($pwd){
+
+    $special_chars = ["!","@","#","$"];
+
+    for ($i=0; $i < strlen($pwd); $i++) { 
+        if (in_array($pwd[$i], $special_chars)) {
+            return true;
+        }
+    }
+    echo "Errore rilevato! La password deve contenere almeno 1 carattere speciale \n";
+    return false;
+}
+
+// checkSpecialChars($password);
+
+
+
+// Funzione unica
+
+function checkPassword($pwd){
+
+    $first_rule = checkLenght($pwd);
+    $second_rule = checkNumber($pwd);
+    $third_rule = checkChars($pwd);
+    $fourth_rule = checkSpecialChars($pwd);
+
+    if ($first_rule && $second_rule && $third_rule && $fourth_rule) {
+        echo "Password accettata";
+    }
+
+}
+
+checkPassword($password);
+
+
+
+
+
+?>
